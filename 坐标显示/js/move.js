f244be0759203e90b01331f5cf56e37fffd1ac6b
Aguang5241 <759229100@qq.com>
2019-10-21 12:44:42 +0800

    add new project

---
+++ 坐标显示/js/move.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+  var box = this.document.getElementsByClassName('box')[0];
+  var show = this.document.getElementsByClassName('show')[0];
+  var spot = this.document.getElementById('spot');
+  var lineX = this.document.getElementById('lineX');
+  var lineY = this.document.getElementById('lineY');
+  var x;
+  var y;
+  box.onmousemove = function(event) {
+    // console.log(event)
+    x = event.clientX;
+    y = event.clientY;
+    show.innerHTML = "坐标：(" + x + "," + y + ")";
+    spot.style.top = y + 'px';
+    spot.style.left = x + 'px';
+    lineX.style.top = y + 2 + 'px';
+    lineY.style.left = x + 2 + 'px';
+  }
+}
