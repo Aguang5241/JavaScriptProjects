d5fba0be9c38e313a8f9358464081b96f12494cf
Aguang5241 <759229100@qq.com>
2019-10-26 19:01:05 +0800

    some modification

--- 赛车/js/street.js
+++ 赛车/js/street.js
@@ -2,10 +2,15 @@ window.onload = function () {
   alert('使用左右方向键或者A,D字母控制小车左右移动\n\n点击确定按钮开始游戏')
   // 捕获背景（父元素）
   var bg = this.document.getElementById('bg');
+  var background = this.document.getElementById('background');
   // 获取屏幕高度,赋予背景
-  var h = this.document.documentElement.clientHeight;
+  var h = this.document.documentElement.clientHeight / 1.25;
   var w = bg.clientWidth;
+  var carsBg = ['url(../img/block1.png)'];
   bg.style.height = h + 'px';
+  background.style.height = document.documentElement.clientHeight + 'px';
+  var score = this.document.getElementById('score');
+  var timeLast = this.document.getElementById('timeLast');
   // 生成box
   function createBox() {
     var box = this.document.createElement('div')
@@ -24,35 +29,67 @@ window.onload = function () {
     } else {
       box.style.left = i + (3 * boxw) + (6 * deltai) + 'px';
     };
-    // box.style.left = Math.floor(Math.random() * 270) + 'px'; // 此处无法修改270
+    box.style.background = carsBg[Math.floor(Math.random() * carsBg.length)];
     bg.appendChild(box);
-    // console.log(box.clientWidth);
     moveBox(box); // 为每个box绑定对象
-    end(box);
+    var carNum = document.querySelectorAll('.box').length;
+    if (carNum < 6) {
+      intervalTime -= 50;
+      clearInterval(timer);
+      if (intervalTime < 0) {
+        intervalTime = 10;
+      };
+      if (intervalTime > 0) {
+        timer = setInterval(add, intervalTime);
+        // console.log(intervalTime);
+      };
+    };
   };
-  // 每隔t毫秒向父元素添加一个box
-  var t = 1500;
-  function add() {
-    // console.log('add');
-    createBox();
-  };
+  // 统计游戏时间
+  var timeLastNum = 0;
   setInterval(function () {
-    add();
-  }, t);
+    timeLastNum++;
+    timeLast.innerHTML = 'Time: ' + timeLastNum + 's';
+  }, 1000)
+  // 当屏幕数量的车辆小于3辆时生成车辆
+  // 初始每隔1s判断数量，添加小车，每次最多添加3辆
+  // 每当车辆总数小于3时，同时减少生成车辆的时间间隔
+  var intervalTime = 2000;
+  // 需要正确关闭！！！
+  function add() {
+    createBox(); // 生成第1辆
+    createBox(); // 生成第2辆
+    createBox(); // 生成第3辆
+  }
+  var timer = setInterval(add, intervalTime);
   // 使得每个box运动起来
   var deltaX = 1;
-  var time = 10;
+  var time = 5;
+  var scoreNum = 0;
   function moveBox(obj) {
-    deltaX += 0.1;
+    deltaX += 0.01; // 加速度运动
+    // console.log(deltaX)
     var pos = 0;
     var id = setInterval(move, time);
     function move() {
-      if (pos == (h - boxh)) {
-        clearInterval(id);
-        bg.removeChild(obj); // 移除运动到底的box
+      if (pos > (h - boxh * 1.2)) {
+        pos = h - boxh * 1.2;
+        if (pos == (h - boxh * 1.2)) {
+          clearInterval(id);
+          bg.removeChild(obj); // 移除运动到底的box
+          scoreNum++;
+          score.innerHTML = 'Score: ' + scoreNum;
+        }
       } else {
         pos += deltaX;
         obj.style.top = pos + 'px';
+        // 死亡判断！！！
+        if (obj.offsetTop > h - 10 - 2 * boxh) {
+          if ((mybox.offsetLeft > obj.offsetLeft - boxw) && (mybox.offsetLeft < obj.offsetLeft + boxw)) {
+            alert('游戏结束\n最终得分：' + scoreNum + '分' + '\n存活时间：' + timeLastNum + 's');
+            window.location.reload();
+          }
+        }
       }
     }
   };
@@ -114,24 +151,32 @@ window.onload = function () {
         break;
     }
   }, sensitivity);
-  // 边缘判断
-  function end(obj) {
-    var v = deltaX / time;
-    var x = h - 10 - 2 * boxh;
-    var t = x / v;
-    setTimeout(function () {
-      var deltaMin = obj.offsetLeft;
-      var deltaMax = obj.offsetLeft + boxw;
-      // console.log(deltaMin);
-      // console.log(deltaMax);
-      // console.log(mybox.offsetLeft);
-      if (mybox.offsetLeft <= deltaMax && mybox.offsetLeft >= deltaMin) {
-        alert('Game End');
-        window.location.reload();
-      }
-    }, t + 300);
-  };
+  // 边缘判断 ——————> 已改良
+  // function getPos(obj) {
+  //   // setInterval(function() {
+  //   //   var left = obj.offsetLeft;
+  //   //   var top = obj.offsetTop;
+  //   //   console.log(left+ ' '+top);
+  //   // }, 10)
+  // }
+  // function end(obj) {
+  //   var v = deltaX / time;
+  //   var x = h - 10 - 2 * boxh;
+  //   var t = x / v;
+  //   setTimeout(function () {
+  //     var deltaMin = obj.offsetLeft;
+  //     var deltaMax = obj.offsetLeft + boxw;
+  //     // console.log(deltaMin);
+  //     // console.log(deltaMax);
+  //     // console.log(mybox.offsetLeft);
+  //     if (mybox.offsetLeft <= deltaMax && mybox.offsetLeft >= deltaMin) {
+  // alert('Game End');
+  // window.location.reload();
+  //     }
+  //   }, t + 300);
+  // };
   // 绑定键盘事件
+  var dir = 0;
   this.document.onkeydown = function (event) {
     event = event || window.event;
     // up 38 w 87；down 40 s 83；left 37 a 65；right 39 d 68
